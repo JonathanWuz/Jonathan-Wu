af9aefbd357ac433628ba18cc281ce2a8b6c5274
JonathanWuz <111464948+JonathanWuz@users.noreply.github.com>
2022-11-03 09:44:07 -0700

    Update Calculator.js

--- _notebooks/Calculator.js
+++ _notebooks/Calculator.js
@@ -79,12 +79,12 @@ class Calculator {
 }
 const numberButtons = document.querySelectorAll(‘[data-number]’)
 const operationButtons = document.querySelectorAll(‘[data-operation]’)
-Const equalsButton = document.querySelector(‘[data-equals]’)
-Const deleteButton = document.querySelector(‘[data-delete]’)
-Const allClearButton = document.querySelector(‘[data-all-clear]’)
-Const previousOperandTextElement = document.querySelector(‘[data-previous-operand]’)
-Const currentOperandTextElement = document.querySelector(‘[data-current-operand]’)
-Const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+const equalsButton = document.querySelector(‘[data-equals]’)
+const deleteButton = document.querySelector(‘[data-delete]’)
+const allClearButton = document.querySelector(‘[data-all-clear]’)
+const previousOperandTextElement = document.querySelector(‘[data-previous-operand]’)
+const currentOperandTextElement = document.querySelector(‘[data-current-operand]’)
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 numberButtons.forEach(button => {
 	button.addEventListener(‘click’, () => {
 	calculator.appendNumber(button.innerText)
